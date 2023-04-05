00c559166f2d56911d405412bff4ca8831727787
Toby Chambers <74092151+thepiex@users.noreply.github.com>
2023-04-05 22:35:21 +0100

    REST Refactor

--- src/messaging/handlers/READY.ts
+++ src/messaging/handlers/READY.ts
@@ -1,22 +1,28 @@
-import { Routes } from '@fawkes.js/api-types'
-import { type Client } from '../../Client'
-import { Application } from '../../structures/Application'
+import { Routes } from '@fawkes.js/api-types';
+import { type Client } from '../../Client';
+import { Application } from '../../structures/Application';
+import { DiscordAPIApplication } from '@fawkes.js/api-types';
 
 export class READY {
-  client: Client
-  constructor (client: Client) {
-    this.client = client
+  client: Client;
+  constructor(client: Client) {
+    this.client = client;
   }
 
-  initialize (): void {
+  initialize(): void {
     this.client.on('READY', (packet) => {
       void (async (packet) => {
-        const application = await this.client.rest.request(Routes.application())
-        await this.client.cache.set('application', application)
-        await this.client.cache.set('ready', packet)
-        this.client.application = new Application(this.client, application)
-        this.client.emit('ready', packet)
-      })(packet)
-    })
+        const application = await this.client.rest.request(
+          Routes.application()
+        );
+        await this.client.cache.set('application', application);
+        await this.client.cache.set('ready', packet);
+        this.client.application = new Application(
+          this.client,
+          <DiscordAPIApplication>application
+        );
+        this.client.emit('ready', packet);
+      })(packet);
+    });
   }
 }
--- src/structures/ChatInputCommandInteraction.ts
+++ src/structures/ChatInputCommandInteraction.ts
@@ -1,140 +1,187 @@
-import { type DiscordAPIGuild, type DiscordAPIButtonComponentButtonStyleType, type DiscordAPIEmoji, type DiscordAPIMessageComponentType, Routes, DiscordAPICommandOptionType, type DiscordAPIInteraction } from '@fawkes.js/api-types'
-import { type Client } from '../Client'
-import { User } from './User'
-import { Message } from './Message'
-import { APIEmbed, type Embed } from './APIEmbed'
-import { BaseInteraction } from './BaseInteraction'
+import {
+  type DiscordAPIGuild,
+  type DiscordAPIButtonComponentButtonStyleType,
+  type DiscordAPIEmoji,
+  type DiscordAPIMessageComponentType,
+  Routes,
+  DiscordAPICommandOptionType,
+  type DiscordAPIInteraction,
+  DiscordAPIMessage,
+} from '@fawkes.js/api-types';
+import { type Client } from '../Client';
+import { User } from './User';
+import { Message } from './Message';
+import { APIEmbed, type Embed } from './APIEmbed';
+import { BaseInteraction } from './BaseInteraction';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Attachment {}
 
 interface ButtonComponent {
-  type: 2
-  style: DiscordAPIButtonComponentButtonStyleType
-  label?: string
-  emoji?: DiscordAPIEmoji
-  custom_id?: string
-  url?: string
-  disabled?: boolean
+  type: 2;
+  style: DiscordAPIButtonComponentButtonStyleType;
+  label?: string;
+  emoji?: DiscordAPIEmoji;
+  custom_id?: string;
+  url?: string;
+  disabled?: boolean;
 }
 
 interface StringSelectComponentOption {
-  label: string
-  value: string
-  description?: string
-  emoji?: DiscordAPIEmoji
+  label: string;
+  value: string;
+  description?: string;
+  emoji?: DiscordAPIEmoji;
 }
 interface StringSelectComponent {
-  type: DiscordAPIMessageComponentType.StringSelect
-  custom_id: string
-  options: StringSelectComponentOption[]
+  type: DiscordAPIMessageComponentType.StringSelect;
+  custom_id: string;
+  options: StringSelectComponentOption[];
 }
 
 interface TextInputComponent {
-  type: DiscordAPIMessageComponentType.TextInput
+  type: DiscordAPIMessageComponentType.TextInput;
 }
 
 interface UserSelectComponent {
-  type: DiscordAPIMessageComponentType.UserSelect
+  type: DiscordAPIMessageComponentType.UserSelect;
 }
 
 interface RoleSelectComponent {
-  type: DiscordAPIMessageComponentType.RoleSelect
+  type: DiscordAPIMessageComponentType.RoleSelect;
 }
 
 interface MentionableSelectComponent {
-  type: DiscordAPIMessageComponentType.MentionableSelect
+  type: DiscordAPIMessageComponentType.MentionableSelect;
 }
 
 interface ChannelSelectComponent {
-  type: DiscordAPIMessageComponentType.ChannelSelect
+  type: DiscordAPIMessageComponentType.ChannelSelect;
 }
 
 interface ActionRowComponent {
-  type: DiscordAPIMessageComponentType.ActionRow
-  components: Array<ButtonComponent | StringSelectComponent | TextInputComponent | UserSelectComponent | RoleSelectComponent | MentionableSelectComponent | ChannelSelectComponent>
+  type: DiscordAPIMessageComponentType.ActionRow;
+  components: Array<
+    | ButtonComponent
+    | StringSelectComponent
+    | TextInputComponent
+    | UserSelectComponent
+    | RoleSelectComponent
+    | MentionableSelectComponent
+    | ChannelSelectComponent
+  >;
 }
 
 interface InteractionResponseOptions {
-  content?: string
-  embeds?: Embed[]
-  attachments?: Attachment[]
-  fetchReply?: boolean
-  components?: ActionRowComponent[]
+  content?: string;
+  embeds?: Embed[];
+  attachments?: Attachment[];
+  fetchReply?: boolean;
+  components?: ActionRowComponent[];
 }
 
 export class ChatInputCommandInteraction extends BaseInteraction {
-  command: string
-  options: any[]
-  constructor (client: Client, interaction: DiscordAPIInteraction,guild:DiscordAPIGuild) {
-    super(client, interaction,guild)
-    const options: any[] = []
+  command: string;
+  options: any[];
+  constructor(
+    client: Client,
+    interaction: DiscordAPIInteraction,
+    guild: DiscordAPIGuild
+  ) {
+    super(client, interaction, guild);
+    const options: any[] = [];
 
-    if ((interaction.data?.options) != null) {
-      interaction.data?.options.forEach( async (option) => {
+    if (interaction.data?.options != null) {
+      interaction.data?.options.forEach(async (option) => {
         switch (option.type) {
           case DiscordAPICommandOptionType.SubCommand:
-            break
+            break;
           case DiscordAPICommandOptionType.SubCommandGroup:
-            break
+            break;
           case DiscordAPICommandOptionType.String:
-            options.push({ name: option.name, data: option.value, type: option.type })
-            break
+            options.push({
+              name: option.name,
+              data: option.value,
+              type: option.type,
+            });
+            break;
           case DiscordAPICommandOptionType.Integer:
-            break
+            break;
           case DiscordAPICommandOptionType.Boolean:
-            break
+            break;
           case DiscordAPICommandOptionType.User:
-            const member = guild.members.find((m) => m.user?.id === option.value)
+            const member = guild.members.find(
+              (m) => m.user?.id === option.value
+            );
 
-            member?.user ? options.push({ name: option.name, data: new User(member.user), type: option.type }) : null
-            break
+            member?.user
+              ? options.push({
+                  name: option.name,
+                  data: new User(member.user),
+                  type: option.type,
+                })
+              : null;
+            break;
           case DiscordAPICommandOptionType.Channel:
-            break
+            break;
           case DiscordAPICommandOptionType.Role:
-            break
+            break;
           case DiscordAPICommandOptionType.Mentionable:
-            break
+            break;
           case DiscordAPICommandOptionType.Number:
-            break
+            break;
           case DiscordAPICommandOptionType.Attachment:
-            break
+            break;
         }
-      })
+      });
     }
 
-    this.command = <string>interaction.data?.name
+    this.command = <string>interaction.data?.name;
 
-    this.options = ((interaction.data?.options) != null) ? options : []
+    this.options = interaction.data?.options != null ? options : [];
   }
 
-  async reply (data: InteractionResponseOptions): Promise<any> {
-    function APIEmbeds (embeds: Embed[]): APIEmbed[] {
-      const APIEmbeds: APIEmbed[] = []
-      embeds.map((embed) => APIEmbeds.push(new APIEmbed(embed)))
-      return APIEmbeds
+  async reply(data: InteractionResponseOptions): Promise<any> {
+    function APIEmbeds(embeds: Embed[]): APIEmbed[] {
+      const APIEmbeds: APIEmbed[] = [];
+      embeds.map((embed) => APIEmbeds.push(new APIEmbed(embed)));
+      return APIEmbeds;
     }
 
-    const APIData: any = { ...data }
-    APIData.embeds = (data.embeds != null) ? APIEmbeds(data.embeds) : []
+    const APIData: any = { ...data };
+    APIData.embeds = data.embeds != null ? APIEmbeds(data.embeds) : [];
 
-    await this.client.rest.request(Routes.createInteractionResponse(this.id, this.token), { type: 4, data: APIData })
+    await this.client.rest.request(
+      Routes.createInteractionResponse(this.id, this.token),
+      { type: 4, data: APIData }
+    );
 
     const fetchReply = async (): Promise<Message> => {
-      return new Message(this.client, await this.client.rest.request(Routes.getOriginalInteractionResponse(this.client?.application?.id, this.token)))
-    }
-    if (data.fetchReply === true) return await fetchReply()
-    else return null
+      return new Message(
+        this.client,
+        <DiscordAPIMessage>(
+          await this.client.rest.request(
+            Routes.getOriginalInteractionResponse(
+              this.client?.application?.id,
+              this.token
+            )
+          )
+        )
+      );
+    };
+    if (data.fetchReply === true) return await fetchReply();
+    else return null;
   }
 
-  getUser (): User[] {
-    const users: any[] = []
+  getUser(): User[] {
+    const users: any[] = [];
 
     this.options.forEach((option): void => {
-      if (option.type === DiscordAPICommandOptionType.User) users.push(option.data)
-    })
-    return users
+      if (option.type === DiscordAPICommandOptionType.User)
+        users.push(option.data);
+    });
+    return users;
   }
 
-  getOption (name: string): void { }
+  getOption(name: string): void {}
 }
